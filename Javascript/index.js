e4921b5a1e25168ee40b1fda2b5a134abde06034
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-05-23 18:49:57 +0200

    added a current location+JS

--- Javascript/index.js
+++ Javascript/index.js
@@ -22,6 +22,7 @@ function updateTime() {
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if(cityTimeZone === "current"){cityTimeZone =moment.tz.guess()};
     let cityName = cityTimeZone.split("/")[1].replace("_", " ");
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector('#cities');
